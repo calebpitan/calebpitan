1dca353075a235eef096614045163009221a00e5
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-07-15 18:33:58 +0100

    removed

--- gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import "./src/components/layouts/layout.global.scss"
-
-import React from "react"
-import BlogWrapper from "./src/components/wrapper"
-
-export const wrapPageElement = ({ element, props }) => (
-  <BlogWrapper {...props}>{element}</BlogWrapper>
-)
-
