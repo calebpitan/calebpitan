71917c92fac972814cd1f9feefa9ad108d4617fb
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-07-28 04:30:20 +0100

    Tape component

---
+++ src/components/tape/index.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+import tape from './tape.mod.scss'
+
+const Tape = ({ small }) => {
+  return (
+    <div className={small ? tape.tapeSmall : tape.tape} aria-hidden="true" />
+  )
+}
+
+Tape.propTypes = {
+  small: PropTypes.bool,
+}
+
+export default Tape
