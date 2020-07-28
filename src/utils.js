6e49fdc37264bcbc591dd151b94fd1050dc5695d
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-07-28 04:28:17 +0100

    utility module

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import flex from './styles/flex.mod.scss'
+import display from './styles/display.mod.scss'
+import screenreader from './styles/screenreader.mod.scss'
+import spacing from './styles/spacing.mod.scss'
+import text from './styles/text.mod.scss'
+import typo from './styles/type.mod.scss'
+
+import classNames from 'classnames/bind'
+
+const utilStyles = {
+  ...flex,
+  ...display,
+  ...screenreader,
+  ...spacing,
+  ...text,
+  ...typo,
+}
+
+export const gcx = (style) => {
+  return classNames.bind(Object.assign(utilStyles, style || {}))
+}
