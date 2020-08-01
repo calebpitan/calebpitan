71622919ed241cbc55af0ba69ffeed49bd61b56f
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-08-01 05:35:25 +0100

    Alternative composition text
    * A component that summarises, emphasize and elaborate a preceeding text or idea

---
+++ src/components/alt-text.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const AltText = ({ children, text, ...rest }) => (
+  <small className="global-alt-text" {...rest}>
+    {children || text }
+  </small>
+)
+
+export default AltText
