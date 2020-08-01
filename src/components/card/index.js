ae2322b7405eddc30156001bda97277fa74a1e6e
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-08-01 05:29:22 +0100

    Add card footer to contain post author, avatar and date

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -11,7 +11,7 @@ import Img from 'gatsby-image'
 
 const cx = gcx(card)
 
-const Card = ({ title, author, avatar, slug, className }) => {
+const Card = ({ title, excerpt, author, avatar, slug, date, className }) => {
   return (
     <div className={cx('card', 'dFlex', 'flexColumn', className)}>
       <div className={cx('cardMain')}>
@@ -20,12 +20,18 @@ const Card = ({ title, author, avatar, slug, className }) => {
             {title}
           </Link>
         </H>
+        {excerpt && <div className={cx('cardExcerpt', 'py2')}>{excerpt}</div>}
       </div>
+
       <div className={cx('dFlex', 'alignItemsCenter', 'cardMeta', 'mtAuto')}>
         <SubjectImage className={cx('cardAvatar')} avatar>
           <Img fluid={avatar.childImageSharp.fluid} draggable={false} />
         </SubjectImage>
-        <span className={cx('cardAuthor')}>{author}</span>
+
+        <div className={cx('dFlex', 'flexColumn', 'mt3')}>
+          <span className={cx('cardFooter', 'flexShrink0')}>{author}</span>
+          <span className={cx('cardFooter', 'flexShrink0')}>{date}</span>
+        </div>
       </div>
     </div>
   )
