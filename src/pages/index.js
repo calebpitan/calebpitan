825a44d7cafdc28a7bcb3ba9abea045402566c28
Caleb Adepitan <36812893+calebpitan@users.noreply.github.com>
2020-08-03 02:10:17 +0100

    Updated
    * Extract frequent queries into a separate module/component/hook
    * Remove unnecessary and excessive spacing by left margin
    * Replace `StuntList` with a general and more reusable `RecentPost` compo.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import Img from 'gatsby-image'
-import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -12,37 +11,22 @@ import H from '../components/heading'
 import Intro from './intro.md'
 import FindMeOn from '../components/find-me-on'
 import Hr from '../components/hr'
-import StuntList from '../components/stunt-list'
+import RecentPost from '../components/recent-post'
 import AltText from '../components/alt-text'
 import { useAvatar } from '../components/hooks'
 import { gcx } from '../utils'
 
 const cx = gcx()
 
-const IndexPage = ({ data }) => {
+const IndexPage = () => {
   const { avatar } = useAvatar()
-  const { allMdx } = data
-
-  const stunts = allMdx.edges.map(
-    ({
-      node: {
-        frontmatter: { title, author, date },
-        fields: { slug },
-      },
-    }) => ({
-      title,
-      author,
-      date,
-      slug,
-    })
-  )
 
   return (
     <Layout>
       <SEO title="Caleb Pitan" isHome />
 
       <div className={cx('dMdFlex')}>
-        <div>
+        <div className={cx('px3')}>
           <SubjectImage>
             <Img fluid={avatar.childImageSharp.fluid} draggable={false} />
           </SubjectImage>
@@ -66,15 +50,7 @@ const IndexPage = ({ data }) => {
           </div>
         </div>
 
-        <div
-          className={cx(
-            'compact-content',
-            'flexMdShrink1',
-            'mtMd5',
-            'mlMd3',
-            'px3'
-          )}
-        >
+        <div className={cx('compact-content', 'flexMdShrink1', 'mtMd5', 'px3')}>
           <H as="2" style={{ margin: 0 }}>
             Hola, bienvenido!
           </H>
@@ -84,7 +60,7 @@ const IndexPage = ({ data }) => {
           <Hr />
           <H as="4">Recent Stunts</H>
           <Tape small />
-          <StuntList data={stunts} />
+          <RecentPost />
         </div>
 
         <div
@@ -95,17 +71,5 @@ const IndexPage = ({ data }) => {
     </Layout>
   )
 }
-
-export const pageQuery = graphql`
-  query {
-    allMdx(
-      sort: { fields: frontmatter___date, order: DESC }
-      limit: 6
-      filter: { frontmatter: { publish: { eq: true } } }
-    ) {
-      ...AllMdxFrag
-    }
-  }
-`
 
 export default IndexPage
