2d8eab2f32e7ea072acbc448c5bbe8702e4c5343
Caleb Adepitan <calebpitan@outlook.com>
2020-08-16 21:46:53 +0100

    Add tag page

---
+++ src/pages/tag.js
@@ -0,0 +1,61 @@
+import * as React from 'react'
+import kebabCase from 'lodash/kebabCase'
+import { graphql, Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import H from '../components/heading'
+import Tape from '../components/tape'
+import { gcx } from '../utils'
+
+const cx = gcx()
+
+const sortAtoZ = (a, b) =>
+  a.fieldValue.toLowerCase() > b.fieldValue.toLowerCase() ? 1 : -1
+
+const Tag = ({
+  data: {
+    allMdx: { group },
+  },
+}) => {
+  return (
+    <Layout>
+      <SEO title={`Tag`} />
+      <div className={cx('global-compact--layout', 'p5')}>
+        <H as="1">Tag</H>
+        <Tape />
+
+        <div>
+          <ul className={cx('dFlex', 'flexWrap', 'justifyContentEvenly', 'listUnstyled')}>
+            {group.sort(sortAtoZ).map((tag) => (
+              <li
+                className={cx('flexShrink0', 'mr3', 'my-3')}
+                key={tag.fieldValue}
+              >
+                <Link
+                  to={`/tags/${kebabCase(tag.fieldValue)}/`}
+                  style={{ padding: `0.4rem 1rem` }}
+                >
+                  {tag.fieldValue} ({tag.totalCount})
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allMdx(limit: 2000, filter: { frontmatter: { publish: { eq: true } } }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
+
+export default Tag
