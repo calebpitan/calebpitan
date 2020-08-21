784f65e1c0fd54c4930e58a03eda0d2872982911
Caleb Adepitan <calebpitan@outlook.com>
2020-08-21 16:04:31 +0100

    Force Update `Icon` component
    * Icon component is SSR rendered in the context of the `currentTheme`
    from `ThemeContext` which is dark by default and in the SSR stage.
    
    * Client side, when hydration occurs, React does a render and don't see
    any changes in the virtual DOM when constructed because this was initially
    prerendered in a context (server) React is oblivious to (client).
    
    * Using `key={...}` in combination with `setIconKey` in `React.useEffect`
    forces a rerender after components mount, making React to update the DOM
    with the correct Icon that was rendered.

--- src/components/theme-switch/index.js
+++ src/components/theme-switch/index.js
@@ -10,33 +10,51 @@ import { ThemeContext, useThemeToggle } from '../wrapper'
 import { THEMES } from '../../utils'
 
 import tswitch from './switch.mod.scss'
+const icons = {
+  light: MdBrightnessHigh,
+  dark: MdBrightness4,
+  auto: MdBrightnessAuto,
+}
 
 const ThemeSwitch = () => {
   const currentTheme = React.useContext(ThemeContext)
   const setCurrentTheme = useThemeToggle()
+  const [iconKey, setIconKey] = React.useState(currentTheme)
 
-  const getNextAndForwardedNextTheme = () => {
+  React.useEffect(() => {
+    /**
+     * ### Force Update `Icon` component.
+     *
+     * Icon component is SSR rendered in the context of the `currentTheme`
+     * from `ThemeContext` which is dark by default and in the SSR stage.
+     *
+     * Client side, when hydration occurs, React does a render and don't see
+     * any changes in the virtual DOM when constructed because this was initially
+     * prerendered in a context (server) React is oblivious to (client).
+     *
+     * Using `key={...}` in combination with `setIconKey` in `React.useEffect`
+     * forces a rerender after components mount, making React to update the DOM
+     * with the correct Icon that was rendered.
+     *
+     */
+    setIconKey(Math.random())
+  }, [])
+
+  const getNextTheme = () => {
     const orderOfThemeSelection = Object.values(THEMES)
     const indexOfNextTheme =
       (orderOfThemeSelection.indexOf(currentTheme) + 1) %
       orderOfThemeSelection.length
     const nextTheme = orderOfThemeSelection[indexOfNextTheme]
-    const forwardedNextTheme = orderOfThemeSelection[indexOfNextTheme]
-    return { nextTheme, forwardedNextTheme }
+    return nextTheme
   }
 
   const switchTheme = () => {
-    const { nextTheme } = getNextAndForwardedNextTheme()
+    const nextTheme = getNextTheme()
     setCurrentTheme(nextTheme)
   }
 
-  const { forwardedNextTheme } = getNextAndForwardedNextTheme()
-
-  const icons = {
-    light: MdBrightnessHigh,
-    dark: MdBrightness4,
-    auto: MdBrightnessAuto,
-  }
+  const nextTheme = getNextTheme()
 
   const ThemeIcon = icons[currentTheme]
 
@@ -46,9 +64,9 @@ const ThemeSwitch = () => {
         <Button
           className={tswitch.switchButton}
           onClick={switchTheme}
-          aria-label={`Switch color scheme to ${forwardedNextTheme}`}
+          aria-label={`Switch color scheme to ${nextTheme}`}
         >
-          <ThemeIcon role="presentation" />
+          <ThemeIcon aria-hidden="true" key={iconKey} />
         </Button>
       </div>
     </>
