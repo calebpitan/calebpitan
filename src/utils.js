eeeec1031d6c90378979dba8d85a818b26db81e7
Caleb Adepitan <calebpitan@outlook.com>
2020-08-26 12:48:14 +0100

    Add more utility functions as needed

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,8 @@ import typo from './styles/type.mod.scss'
 
 import classNames from 'classnames/bind'
 
+export const FIREBASE_CLOUD_FUNCTIONS_BASEURL = `https://us-central1-caleb-97b50.cloudfunctions.net/app`
+
 const utilStyles = {
   ...flex,
   ...display,
@@ -35,4 +37,23 @@ export const getTheme = () => {
 
 export const gcx = (style) => {
   return classNames.bind(Object.assign({}, utilStyles, style || {}))
+}
+
+export const largeNumber = (num) => {
+  const baseExp = 3
+  const units = {
+    '3': 'K',
+    '6': 'M',
+    '9': 'G',
+    '12': 'T',
+  }
+
+  let exp = Math.floor(Math.log10(num))
+  exp -= exp % baseExp
+
+  if (!num) return
+  if (num < 10 ** baseExp) return num
+
+  const reducedNumber = num / 10 ** exp
+  return `${reducedNumber.toFixed(1)}${units[exp]}`
 }
