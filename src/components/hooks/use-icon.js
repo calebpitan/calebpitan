47573c68d7ff939a4ab16536be2b6a82f041237a
Caleb Adepitan <calebpitan@outlook.com>
2020-10-12 15:07:01 +0100

    Update icon filename

--- src/components/hooks/use-icon.js
+++ src/components/hooks/use-icon.js
@@ -3,7 +3,7 @@ import { graphql, useStaticQuery } from 'gatsby'
 const useIcon = () => {
   const data = useStaticQuery(graphql`
     query {
-      icon: file(relativePath: { eq: "caleb.jpg" }) {
+      icon: file(relativePath: { eq: "ninja.png" }) {
         childImageSharp {
           fixed(width: 300, quality: 100) {
             ...GatsbyImageSharpFixed
