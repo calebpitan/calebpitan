3f5fd9a0f277183955b15780f12a14b05f3a4875
Caleb Adepitan <calebpitan@gmail.com>
2023-03-19 21:49:49 +0100

    chore: update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        implementation: require('node-sass'),
+        implementation: require('sass'),
         // Override the file regex for SASS
         sassRuleTest: /\.global\.s(a|c)ss$/,
         // Override the file regex for CSS modules
